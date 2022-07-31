62e9f90a20ec16142ae0bffb9286d0bc94885b0f
Muhammad Rifky Irfan Zarkasyi <70007191+Irfanzarkasyi@users.noreply.github.com>
2022-08-01 02:11:34 +0700

    Add files via upload

---
+++ includes/HTML-head.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="description" content="A IRA Website" />
+        <meta name="keywords" content="IRA, , forum" >
+  
+        <title><?php echo TITLE; ?></title>
+        
+        <link href="css/bootstrap.min.css" rel="stylesheet"> 
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet"> 
+        <link href="css/footer.css" rel="stylesheet">
+        <link href="css/styling.css" rel="stylesheet">
+        
+        
+        
+        
+        <link rel="shortcut icon" href="img/LOGO.png" />
